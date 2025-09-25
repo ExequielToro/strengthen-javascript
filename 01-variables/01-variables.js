2e6b0beb4754b6598f3dbf47f77a56b94f39fc05
ExequielToro <178219965+ExequielToro@users.noreply.github.com>
2025-09-25 00:00:59 -0300

    feat: mini proyecto JS interactivo con var, let, const

--- 01-variables/01-variables.js
+++ 01-variables/01-variables.js
@@ -8,7 +8,7 @@ Cada palabra siguiente empieza con mayúscula.
 
 No se usan espacios ni guiones bajos.
 */
-
+/*
 var helloWorld = " Hello js "
 console.log(helloWorld)
 
@@ -32,5 +32,40 @@ var también permite re-declarar la misma variable sin error:
 
 // let
 
+/*let helloWorld2 = " Hello js 2 "
+console.log(helloWorld2)
+
+helloWorld2 = " Hello js de nuevo 2"
+console.log(helloWorld2)
+
 
 // const
+
+const helloWorld3 = " Hello js 3 "
+console.log(helloWorld3) 
+
+//error
+
+//helloWorld3 = " Hello js de nuevo 3"
+//console.log(helloWorld3)*/
+
+//mini-saludos
+
+// Declaramos variables
+/*var nombre = prompt("Ingresa tu nombre:");   // puede cambiar cada vez que el usuario escribe
+let edad = prompt("Ingresa tu edad:");      // let porque puede cambiar
+const ciudad = "Santiago";                  // ciudad fija
+
+// Mostramos en consola
+console.log("Hola " + nombre + "!");
+console.log("Tienes " + edad + " años");
+console.log("Vives en " + ciudad);*/
+
+
+var nombre = prompt("Ingresa tu nombre:");
+let edad = prompt("Ingresa tu edad:");
+const ciudad = "Santiago";
+
+document.write("Hola " + nombre + "!<br>");
+document.write("Tienes " + edad + " años<br>");
+document.write("Vives en " + ciudad);
