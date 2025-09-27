e4737b9664105cbeed9ae3d3f08fb13454dc85f3
ExequielToro <178219965+ExequielToro@users.noreply.github.com>
2025-09-27 00:52:16 -0300

    feat: incluir string, number, boolean, null, undefined, symbol y bigint en datatypes.js

---
+++ 01-variables/02-datatypes.js
@@ -0,0 +1,53 @@
+// Cadena de texto ( String)
+
+let myname = "Pablo Toro"
+let alias = "Pablete"
+let email = "Pablotoro@gmail.com"
+
+// numeros (number)
+
+let age = 37 // entero
+let height = 1.63 //decimal
+
+// Booleanos( boolean)
+
+let isTeacher= true
+let isStudent= false
+
+// Underfiend
+
+let undefinedValue
+console.log(undefinedValue)
+
+// Null
+let nullValue = null
+
+// Symbol
+
+let mySimbol = Symbol("mysymbol")
+
+// BigInt
+
+let myBigInt = BigInt(188989898989898989818181)
+let myBigInt2 = 898989898989898n
+
+// Mostramos los tipos de datos
+
+
+console.log(typeof myname)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySimbol)
+console.log(typeof myBigInt)
